f438b3e3feb24ebad9ef7a0d7da36f8ab1afffe8
Danny Phillips <dannyp@procore.com>
2015-11-16 21:31:33 -0800

    added deploy tasks

---
+++ utils/deploy.js
@@ -0,0 +1,27 @@
+import GitRepo from 'git-repository';
+import task from './task';
+
+const remote = {
+  name: 'github',
+  url: 'https://github.com/dannyphillips/gatsby-site.git',
+  branch: 'gh-pages',
+};
+
+export default task(async function deploy() {
+  // Initialize a new Git repository inside the `/public` folder
+  // if it doesn't exist yet
+  const repo = await GitRepo.open('public', { init: true });
+  await repo.setRemote(remote.name, remote.url);
+
+  // Fetch the remote repository if it exists
+  if ((await repo.hasRef(remote.url, remote.branch))) {
+    await repo.fetch(remote.name);
+    await repo.reset(`${remote.name}/${remote.branch}`, { hard: true });
+    await repo.clean({ force: true });
+  }
+
+  // Push the contents of the public folder to the remote server via Git
+  await repo.add('--all .');
+  await repo.commit('Update ' + new Date().toISOString());
+  await repo.push(remote.name, 'master:' + remote.branch);
+});
