02c979d3608a42c76e045a4e3afc573b63f0dbcf
Danny Phillips <dannyp@procore.com>
2015-11-18 08:43:33 -0800

    changed react component

--- pages/docs/some-react-code/index.jsx
+++ pages/docs/some-react-code/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Demo from './_Demo';
+import CountdownClock from './_Demo';
 import DocumentTitle from 'react-document-title';
 
 module.exports = React.createClass({
@@ -16,25 +16,13 @@ module.exports = React.createClass({
     return (
       <DocumentTitle title={`${module.exports.metadata().title} | ${this.props.config.siteTitle}`}>
         <div>
-          <h1>Some React Code</h1>
-          <p>
-            It's easy to intermix different file types. The other documentation pages
-            are all written in Markdown but this page is a normal React.js component.
-            Gatsby has built-in support for Markdown, HTML, JSX, and CJSX (Coffeescript flavored JSX)
-            but it's easy to add support for additional file formats.
-          </p>
-          <p>
-            React.js component pages makes it easy to add interactivity or ajax enhancements
-            to what is otherwise a static site. In the case of a documentation site,
-            it let's you embed sample/runnable code to illustrate your documentation.
-            (This demo is from <a href="https://github.com/chenglou/react-motion">React Motion</a>).
-          </p>
+          <h1>React Countdown Clock</h1>
           <div
             style={{
               height: 500
             }}
           >
-            <Demo/>
+            <CountdownClock/>
           </div>
         </div>
       </DocumentTitle>
